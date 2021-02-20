9bcc738aa9acc6eb50140ee0c10aaa68d9b8190e
anne.yang <anne.yang@mart.blue>
2021-02-20 19:06:08 +0700

    update .gitignore

--- report/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# author: anne
-# datetime: 2020-12-03 10:56
-# filename: MyPython/__init__.py
--- test_case/company_api/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# author: anne
-# datetime: 2021-02-20 17:11
-# filename: test_project/__init__.py
