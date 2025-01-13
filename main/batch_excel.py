2029949c3aa34e8ee31ed77d4fb952d7a1d66917
qianlong.yang <qianlong.yang@tuya.com>
2025-01-13 17:58:37 +0800

    excel批处理某行数据

---
+++ main/batch_excel.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: 千泷（杨婷）
+# @Email: qianlong.yang@tuya.com
+# @Time: 2025/1/13 下午5:20
+# @File: batch_excel
---
+++ main/generate_picture.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: 千泷（杨婷）
+# @Email: qianlong.yang@tuya.com
+# @Time: 2025/1/7 下午6:28
+# @File: generate_picture
